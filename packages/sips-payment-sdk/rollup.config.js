b3db4891362085c65674b2036d199dbabf4ce371
CrlH <10894454+CrlH@users.noreply.github.com>
2019-09-09 13:48:09 +0200

    updated build & plugin configuration

--- packages/sips-payment-sdk/rollup.config.js
+++ packages/sips-payment-sdk/rollup.config.js
@@ -1,7 +1,4 @@
 import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
-import resolve from 'rollup-plugin-node-resolve';
-import { uglify } from 'rollup-plugin-uglify';
 
 const config = [{
   input: 'src/index.js',
@@ -9,35 +6,9 @@ const config = [{
     file: 'dist/sips-payment-sdk.js',
     format: 'cjs',
   },
-  external: ['axios', 'crypto-js/sha256', 'crypto-js/hmac-sha256', '@worldline/sips-payment-dom'],
+  external: ['axios', '@worldline/sips-payment-dom'],
   plugins: [
-    resolve({
-      customResolveOptions: {
-        moduleDirectory: ['../../node_modules', '../'],
-      },
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-    babel({
-      babelrc: false,
-      presets: [
-        ['env', {
-          modules: false,
-          loose: true,
-        }],
-      ],
-      plugins: ['external-helpers', 'transform-class-properties'],
-    }),
-  ],
-}, {
-  input: 'dist/sips-payment-sdk.js',
-  output: {
-    file: 'dist/sips-payment-sdk.min.js',
-    format: 'cjs',
-  },
-  plugins: [
-    uglify(),
+   babel(),
   ],
 }];
 
